d25f56c9ead63df7ec9481de26386a0cfdbd1fd7
Esteban6y6 <Esteban.Rocha@fundacion-jala.org>
2023-07-19 17:47:43 -0400

    tarea 2

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,5 +1,7 @@
 import pygame
 from game.components.enemies.enemy import Enemy
+from game.components.enemies.enemy2 import Enemy2
+from game.components.enemies.enemy3 import Enemy3
 
 class EnemyManager:
     def __init__(self):
@@ -16,6 +18,13 @@ class EnemyManager:
             enemy.draw(screen)
 
     def add_enemy(self):
-        if len(self.enemies) < 1:
-            enemy = Enemy()
-            self.enemies.append(enemy)
+        if len(self.enemies) < 3:
+            if not any(isinstance(enemy, Enemy) for enemy in self.enemies):
+                enemy = Enemy()
+                self.enemies.append(enemy)
+            elif not any(isinstance(enemy, Enemy2) for enemy in self.enemies):
+                enemy2 = Enemy2()
+                self.enemies.append(enemy2)
+            elif not any(isinstance(enemy, Enemy3) for enemy in self.enemies):
+                enemy3 = Enemy3()
+                self.enemies.append(enemy3)
